9cb00bec16785e32edbcbb27f68d480ac44cafce
yuvaprajan11 <151350946+Yuvaprajan@users.noreply.github.com>
2025-02-11 11:44:13 +0530

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,7 @@ function Contact() {
     setLoading(true); // Set loading to true when form submission starts
 
     try {
-      const response = await fetch("https://taskit-backend-4mnv.onrender.com/api/contact", {
+      const response = await fetch("https://taskit-tl8o.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -80,6 +80,17 @@ function Contact() {
                   placeholder="Enter your name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="subject">Subject</label>
+                <input
+                  type="text"
+                  id="subject"
+                  placeholder="Enter subject"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
                   required
                 />
               </div>
